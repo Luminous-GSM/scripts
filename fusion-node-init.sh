7f0db15642041f3e20744bdc103e3d1ddbfdc7de
Marno van Niekerk <marnovn071@gmail.com>
2022-01-06 00:11:50 +0200

    Added restart policy

--- fusion-node-init.sh
+++ fusion-node-init.sh
@@ -24,6 +24,7 @@ POD_NAME="fusion-agent"
 AGENT_VERSION="0.0.1-SNAPSHOT"
 DOCKER_IMAGE="ghcr.io/luminous-gsm/fusion:${AGENT_VERSION}"
 MEMORY_ALLOCATION="256m"
+RESTART_POLICY="always"
 
 #=====> VARIABLES <=====#
 paramters=("$@")
@@ -41,11 +42,12 @@ fi
 
 #=====> AGENT RUN COMMAND <=====#
 if [[ $(docker ps -a --filter="name=${POD_NAME}" --filter "status=exited" | grep -w "${POD_NAME}") ]]; then
-  docker start -a "${POD_NAME}"
+#  docker start -a "${POD_NAME}"
+  echo "Fusion Agent should be starting automatically"
 elif [[ $(docker ps -a --filter="name=${POD_NAME}" --filter "status=running" | grep -w "${POD_NAME}") ]]; then
   echo "Fusion Agent running"
 else
   docker pull "${DOCKER_IMAGE}"
-  docker create -m ${MEMORY_ALLOCATION} -v /var/run/docker.sock:/var/run/docker.sock -e ENV_NODE_NAME="${ENV_NODE_NAME}" -e ENV_NODE_UNIQUE_ID="${ENV_NODE_UNIQUE_ID}" -e ENV_NODE_DESCRIPTION="${ENV_NODE_DESCRIPTION}" -e ENV_NODE_AUTHORIZATION_TOKEN="${ENV_NODE_AUTHORIZATION_TOKEN}" -e ENV_PLATFORM="${ENV_PLATFORM}" -e ENV_NODE_HOSTNAME="${ENV_NODE_HOSTNAME}" -p ${PORT}:${PORT} --name ${POD_NAME} ${DOCKER_IMAGE}
+  docker create -m ${MEMORY_ALLOCATION} -v /var/run/docker.sock:/var/run/docker.sock -e ENV_NODE_NAME="${ENV_NODE_NAME}" -e ENV_NODE_UNIQUE_ID="${ENV_NODE_UNIQUE_ID}" -e ENV_NODE_DESCRIPTION="${ENV_NODE_DESCRIPTION}" -e ENV_NODE_AUTHORIZATION_TOKEN="${ENV_NODE_AUTHORIZATION_TOKEN}" -e ENV_PLATFORM="${ENV_PLATFORM}" -e ENV_NODE_HOSTNAME="${ENV_NODE_HOSTNAME}" -p ${PORT}:${PORT} --name ${POD_NAME} --restart "${RESTART_POLICY}" ${DOCKER_IMAGE}
   docker start -a "${POD_NAME}"
 fi
