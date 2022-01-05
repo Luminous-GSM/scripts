1f700a7a6b66d3ebab22507b3a0ff97d5f368225
Marno van Niekerk <marnovn071@gmail.com>
2022-01-06 01:16:57 +0200

    Added restart policy

--- fusion-node-init.sh
+++ fusion-node-init.sh
@@ -42,15 +42,18 @@ fi
 
 #=====> AGENT RUN COMMAND <=====#
 docker pull "${DOCKER_IMAGE}"
+docker stop "${DOCKER_IMAGE}"
+docker rm "${DOCKER_IMAGE}"
+docker create -m ${MEMORY_ALLOCATION} -v /var/run/docker.sock:/var/run/docker.sock -e ENV_NODE_NAME="${ENV_NODE_NAME}" -e ENV_NODE_UNIQUE_ID="${ENV_NODE_UNIQUE_ID}" -e ENV_NODE_DESCRIPTION="${ENV_NODE_DESCRIPTION}" -e ENV_NODE_AUTHORIZATION_TOKEN="${ENV_NODE_AUTHORIZATION_TOKEN}" -e ENV_PLATFORM="${ENV_PLATFORM}" -e ENV_NODE_HOSTNAME="${ENV_NODE_HOSTNAME}" -p ${PORT}:${PORT} --name ${POD_NAME} --restart "${RESTART_POLICY}" ${DOCKER_IMAGE}
+docker start -a "${POD_NAME}"
 
-if [[ $(docker ps -a --filter="name=${POD_NAME}" --filter "status=exited" | grep -w "${POD_NAME}") ]]; then
+#if [[ $(docker ps -a --filter="name=${POD_NAME}" --filter "status=exited" | grep -w "${POD_NAME}") ]]; then
+#  echo "Fusion Agent should be starting automatically"
+#  docker restart "${POD_NAME}"
+#elif [[ $(docker ps -a --filter="name=${POD_NAME}" --filter "status=running" | grep -w "${POD_NAME}") ]]; then
+#  echo "Fusion Agent running"
+#  docker restart "${POD_NAME}"
+#else
+#  docker create -m ${MEMORY_ALLOCATION} -v /var/run/docker.sock:/var/run/docker.sock -e ENV_NODE_NAME="${ENV_NODE_NAME}" -e ENV_NODE_UNIQUE_ID="${ENV_NODE_UNIQUE_ID}" -e ENV_NODE_DESCRIPTION="${ENV_NODE_DESCRIPTION}" -e ENV_NODE_AUTHORIZATION_TOKEN="${ENV_NODE_AUTHORIZATION_TOKEN}" -e ENV_PLATFORM="${ENV_PLATFORM}" -e ENV_NODE_HOSTNAME="${ENV_NODE_HOSTNAME}" -p ${PORT}:${PORT} --name ${POD_NAME} --restart "${RESTART_POLICY}" ${DOCKER_IMAGE}
 #  docker start -a "${POD_NAME}"
-  echo "Fusion Agent should be starting automatically"
-  docker restart "${POD_NAME}"
-elif [[ $(docker ps -a --filter="name=${POD_NAME}" --filter "status=running" | grep -w "${POD_NAME}") ]]; then
-  echo "Fusion Agent running"
-  docker restart "${POD_NAME}"
-else
-  docker create -m ${MEMORY_ALLOCATION} -v /var/run/docker.sock:/var/run/docker.sock -e ENV_NODE_NAME="${ENV_NODE_NAME}" -e ENV_NODE_UNIQUE_ID="${ENV_NODE_UNIQUE_ID}" -e ENV_NODE_DESCRIPTION="${ENV_NODE_DESCRIPTION}" -e ENV_NODE_AUTHORIZATION_TOKEN="${ENV_NODE_AUTHORIZATION_TOKEN}" -e ENV_PLATFORM="${ENV_PLATFORM}" -e ENV_NODE_HOSTNAME="${ENV_NODE_HOSTNAME}" -p ${PORT}:${PORT} --name ${POD_NAME} --restart "${RESTART_POLICY}" ${DOCKER_IMAGE}
-  docker start -a "${POD_NAME}"
-fi
+#fi
