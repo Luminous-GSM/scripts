93c6749f38210457eae552a68c628eb975d09056
Marno van Niekerk <marnovn071@gmail.com>
2022-01-05 17:28:40 +0200

    Added aws and hostname stuff.

--- fusion-node-init.sh
+++ fusion-node-init.sh
@@ -9,6 +9,15 @@ systemctl enable --now docker
 usermod -a -G docker ec2-user
 chmod 666 /var/run/docker.sock
 
+# Replace the ExecStart with the one below
+SEARCH_STRING="/^ExecStart=/"
+REPLACE_STRING="ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"
+FILE_DIRECTORY="/usr/lib/systemd/system/docker.service"
+sed -i "s@${SEARCH_STRING}@${REPLACE_STRING}@" ${FILE_DIRECTORY}
+
+systemctl daemon-reload
+systemctl restart docker
+
 #=====> CONSTANTS <=====#
 PORT=7878
 POD_NAME="fusion-agent"
@@ -24,9 +33,10 @@ ENV_NODE_AUTHORIZATION_TOKEN=${paramters[3]}
 ENV_PLATFORM=${paramters[4]}
 ENV_NODE_HOSTNAME=${paramters[5]}
 
+# Get the hostname of the EC2 instance
 if [ "$ENV_PLATFORM" == "aws" ]; then
   ENV_NODE_HOSTNAME="$(ec2-metadata --local-hostname | cut -d " " -f 2)"
 fi
 
 #=====> AGENT RUN COMMAND <=====#
-docker run -m ${MEMORY_ALLOCATION} -v /var/run/docker.sock:/var/run/docker.sock -e ENV_NODE_NAME="${ENV_NODE_NAME}" -e ENV_NODE_UNIQUE_ID="${ENV_NODE_UNIQUE_ID}" -e ENV_NODE_DESCRIPTION="${ENV_NODE_DESCRIPTION}" -e ENV_NODE_AUTHORIZATION_TOKEN="${ENV_NODE_AUTHORIZATION_TOKEN}" -e ENV_PLATFORM="${ENV_PLATFORM}" -e ENV_NODE_HOSTNAME="${ENV_NODE_HOSTNAME}" -p ${PORT}:${PORT} --name ${POD_NAME} ${DOCKER_IMAGE}
+docker run -d -m ${MEMORY_ALLOCATION} -v /var/run/docker.sock:/var/run/docker.sock -e ENV_NODE_NAME="${ENV_NODE_NAME}" -e ENV_NODE_UNIQUE_ID="${ENV_NODE_UNIQUE_ID}" -e ENV_NODE_DESCRIPTION="${ENV_NODE_DESCRIPTION}" -e ENV_NODE_AUTHORIZATION_TOKEN="${ENV_NODE_AUTHORIZATION_TOKEN}" -e ENV_PLATFORM="${ENV_PLATFORM}" -e ENV_NODE_HOSTNAME="${ENV_NODE_HOSTNAME}" -p ${PORT}:${PORT} --name ${POD_NAME} ${DOCKER_IMAGE}
